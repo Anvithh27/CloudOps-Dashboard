e6563cf6a2bb1cb123d976fc97ce0e4f83ea2744
Anvith <lokkuanvith@gmail.com>
2026-08-01 17:32:22 +0000

    initial backend commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+// Allow requests from the React frontend
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("CloudOps Backend is running 🚀");
+});
+
+app.get("/api/message", (req, res) => {
+  res.json({
+    message: "Hello from Backend 🚀",
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "UP",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
